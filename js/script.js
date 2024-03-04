5f63814852e5d21d67b03e1476718b8c9d3c2b29
simone-danzeri <s.danzeri@icloud.com>
2024-03-04 14:44:08 +0100

    feat: add userName

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// ACQUISIRE I DATI DELL'UTENTE
+//chiedere all'utente il nome
+let userName = prompt('Ciao! Puoi dirmi il tuo nome?')
+console.log(userName)
+//chiedere all'utente cognome
+//chidere all'utente il colore preferito
+//stampare la pw
