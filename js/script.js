f4bdc876728c47aeaf8a4be991fbde1f76f8e313
simone-danzeri <s.danzeri@icloud.com>
2024-03-04 14:46:52 +0100

    feat: add lastName

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 // ACQUISIRE I DATI DELL'UTENTE
 //chiedere all'utente il nome
-let userName = prompt('Ciao! Puoi dirmi il tuo nome?')
-console.log(userName)
+let userName = prompt('Ciao! Puoi dirmi il tuo nome?');
+//console.log(userName);
+
 //chiedere all'utente cognome
+let userLastname = prompt('Mi servirebbe anche il cognome');
+console.log(userLastname);
+
 //chidere all'utente il colore preferito
 //stampare la pw
