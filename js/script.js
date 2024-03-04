dd282d889017c235f9a83548ab5664678acd85d3
simone-danzeri <s.danzeri@icloud.com>
2024-03-04 14:57:00 +0100

    feat: print new password

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 // ACQUISIRE I DATI DELL'UTENTE
+
 //chiedere all'utente il nome
 let userName = prompt('Ciao! Puoi dirmi il tuo nome?');
 //console.log(userName);
@@ -9,5 +10,9 @@ let userLastname = prompt('Mi servirebbe anche il cognome');
 
 //chidere all'utente il colore preferito
 let userColor = prompt('Qual è il tuo colore preferito?')
-console.log(userColor);
+//console.log(userColor);
+
 //stampare la pw
+let userPassword = `La tua nuova password è: ${userName}${userLastname}${userColor}23`
+document.getElementById('message').innerHTML = userPassword;
+console.log(userPassword);
